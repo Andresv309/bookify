19013b1d9056185a1ee6c24be8c15d0ae09d83e9
Cristian Vega <crisda.arive.1503@gmail.com>
2023-07-13 21:28:07 -0500

    Add app view Pages

---
+++ src/app/app.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app;
+
+import javax.xml.transform.Source;
+
+/**
+ *
+ * @author Cristian Vega
+ */
+public class app {
+    public static void main(String[] args) {
+        
+        Login login = new Login();
+        
+        login.setVisible(true);
+    }
+    
+}
