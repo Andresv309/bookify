73beebb63c6c0fe20eca4eac6d36ab142de451dd
Cristian Vega <crisda.arive.1503@gmail.com>
2023-07-22 23:24:03 -0500

    fix Component card

--- src/app/views/profits/Card.java
+++ src/app/views/profits/Card.java
@@ -1,12 +1,47 @@
 package app.views.profits;
 
-public class Card extends javax.swing.JPanel {
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
+public class Card extends javax.swing.JPanel {
+    
+
+    public Color getColor1() {
+        return color1;
+    }
+
+    public void setColor1(Color color1) {
+        this.color1 = color1;
+    }
+
+    public Color getColor2() {
+        return color2;
+    }
+
+    public void setColor2(Color color2) {
+        this.color2 = color2;
+    }
+
+    private Color color1;
+    private Color color2;
+    
  
     public Card() {
         initComponents();
         setOpaque(false);
+        color1 = Color.BLACK;
+        color2 = Color.WHITE; 
     }
+    
+    public void  setData(Model_Card data) {
+        lbIcon.setIcon(data.getIcon());
+        lbTitle.setText(data.getTitle());
+        lbValues.setText(data.getValues());
+        lbDescription.setText(data.getDescription());
+    } 
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -17,14 +52,25 @@ public class Card extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        lbIcon = new javax.swing.JLabel();
+        lbValues = new javax.swing.JLabel();
+        lbTitle = new javax.swing.JLabel();
+        lbDescription = new javax.swing.JLabel();
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Sales.png"))); // NOI18N
-        jLabel1.setToolTipText("");
+        lbIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Sales.png"))); // NOI18N
+        lbIcon.setToolTipText("");
 
-        jLabel2.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
-        jLabel2.setText("jLabel2");
+        lbValues.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 18)); // NOI18N
+        lbValues.setForeground(new java.awt.Color(255, 255, 255));
+        lbValues.setText("Values");
+
+        lbTitle.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 14)); // NOI18N
+        lbTitle.setForeground(new java.awt.Color(255, 255, 255));
+        lbTitle.setText("Title ");
+
+        lbDescription.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        lbDescription.setForeground(new java.awt.Color(255, 255, 255));
+        lbDescription.setText("Description");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -33,24 +79,47 @@ public class Card extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel1))
-                .addContainerGap(274, Short.MAX_VALUE))
+                    .addComponent(lbDescription)
+                    .addComponent(lbTitle)
+                    .addComponent(lbValues)
+                    .addComponent(lbIcon))
+                .addContainerGap(247, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addComponent(jLabel1)
+                .addGap(20, 20, 20)
+                .addComponent(lbIcon)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addContainerGap(98, Short.MAX_VALUE))
+                .addComponent(lbTitle)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lbValues)
+                .addGap(27, 27, 27)
+                .addComponent(lbDescription)
+                .addContainerGap(27, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        GradientPaint g = new GradientPaint(0, 0, color1, 0, getHeight(), color2);
+        g2.setPaint(g);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        g2.setColor(new Color(255, 255, 255, 50));
+        g2.fillOval(getWidth() - (getHeight() / 2), 10, getHeight(), getHeight());
+        g2.fillOval(getWidth() - (getHeight() / 2) - 20, getHeight() / 2 + 20, getHeight(), getHeight());
+        super.paintComponent(grphcs);
+    }
+    
+    
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lbDescription;
+    private javax.swing.JLabel lbIcon;
+    private javax.swing.JLabel lbTitle;
+    private javax.swing.JLabel lbValues;
     // End of variables declaration//GEN-END:variables
 }
