6dbe14872e60662696015889e028ddc26331608a
Andresv309 <andresv309@gmail.com>
2023-07-23 07:19:41 -0500

    Refactor model to handle all info related to sales

--- src/app/models/Sale.java
+++ src/app/models/Sale.java
@@ -1,18 +1,24 @@
 
 package app.models;
 
-import java.util.List;
+import java.math.BigDecimal;
 import java.util.Objects;
 
 
 public class Sale {
    private Long id = null;
-   private Long idCustomer;
-   private List<Long> idBooks;
+   private Long idBook;
+   private Long idCustomer = null;
+   private BigDecimal salePrice;
+   private Long idSaleParent = null;
+   private String createdAt;
+   private String customerName;
+   private String bookName;
+   
 
-    public Sale(Long idCustomer, List<Long> idBooks) {
-        this.idCustomer = idCustomer;
-        this.idBooks = idBooks;
+    public Sale(Long idBook, BigDecimal salePrice) {
+        this.idBook = idBook;
+        this.salePrice = salePrice;
     }
 
     public Long getId() {
@@ -23,6 +29,14 @@ public class Sale {
         this.id = id;
     }
 
+    public Long getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(Long idBook) {
+        this.idBook = idBook;
+    }
+
     public Long getIdCustomer() {
         return idCustomer;
     }
@@ -31,20 +45,57 @@ public class Sale {
         this.idCustomer = idCustomer;
     }
 
-    public List<Long> getIdBooks() {
-        return idBooks;
+    public BigDecimal getSalePrice() {
+        return salePrice;
     }
 
-    public void setIdBooks(List<Long> idBooks) {
-        this.idBooks = idBooks;
+    public void setSalePrice(BigDecimal salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public Long getIdSaleParent() {
+        return idSaleParent;
+    }
+
+    public void setIdSaleParent(Long idSaleParent) {
+        this.idSaleParent = idSaleParent;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
     }
 
     @Override
     public int hashCode() {
-        int hash = 3;
-        hash = 71 * hash + Objects.hashCode(this.id);
-        hash = 71 * hash + Objects.hashCode(this.idCustomer);
-        hash = 71 * hash + Objects.hashCode(this.idBooks);
+        int hash = 7;
+        hash = 31 * hash + Objects.hashCode(this.id);
+        hash = 31 * hash + Objects.hashCode(this.idBook);
+        hash = 31 * hash + Objects.hashCode(this.idCustomer);
+        hash = 31 * hash + Objects.hashCode(this.salePrice);
+        hash = 31 * hash + Objects.hashCode(this.idSaleParent);
+        hash = 31 * hash + Objects.hashCode(this.createdAt);
+        hash = 31 * hash + Objects.hashCode(this.customerName);
+        hash = 31 * hash + Objects.hashCode(this.bookName);
         return hash;
     }
 
@@ -60,17 +111,32 @@ public class Sale {
             return false;
         }
         final Sale other = (Sale) obj;
+        if (!Objects.equals(this.createdAt, other.createdAt)) {
+            return false;
+        }
+        if (!Objects.equals(this.customerName, other.customerName)) {
+            return false;
+        }
+        if (!Objects.equals(this.bookName, other.bookName)) {
+            return false;
+        }
         if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.idBook, other.idBook)) {
             return false;
         }
         if (!Objects.equals(this.idCustomer, other.idCustomer)) {
             return false;
         }
-        return Objects.equals(this.idBooks, other.idBooks);
+        if (!Objects.equals(this.salePrice, other.salePrice)) {
+            return false;
+        }
+        return Objects.equals(this.idSaleParent, other.idSaleParent);
     }
 
     @Override
     public String toString() {
-        return "Sale{" + "id=" + id + ", idCustomer=" + idCustomer + ", idBooks=" + idBooks + '}';
+        return "Sale{" + "id=" + id + ", idBook=" + idBook + ", idCustomer=" + idCustomer + ", salePrice=" + salePrice + ", idSaleParent=" + idSaleParent + ", createdAt=" + createdAt + ", customerName=" + customerName + ", bookName=" + bookName + '}';
     }
 }
