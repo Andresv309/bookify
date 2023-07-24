77515f49e32ef888c2c033d5850da03e11a08ad2
Cristian Vega <crisda.arive.1503@gmail.com>
2023-07-24 02:30:07 -0500

    Fix MarketPlace Panel

--- src/app/views/marketplace/MarketPlacePanel.java
+++ src/app/views/marketplace/MarketPlacePanel.java
@@ -48,25 +48,24 @@ public class MarketPlacePanel extends javax.swing.JPanel {
     private void initComponents() {
 
         panelBackground1 = new components.mainComponents.swing.PanelBackground();
-        btnNewBook = new components.ButtonGradient();
-        btnOpenShopCart = new javax.swing.JButton();
+        btnOpenShopCart1 = new javax.swing.JButton();
+        btnOpenShopCart = new components.BadgeButton();
         scroll = new javax.swing.JScrollPane();
         portrait = new components.mainComponents.swing.PanelBackground();
 
         setBackground(new java.awt.Color(204, 204, 204));
 
-        btnNewBook.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icon/Book.png"))); // NOI18N
-        btnNewBook.setText("Generar Libro");
-        btnNewBook.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 12)); // NOI18N
-        btnNewBook.setGradientColor1(new java.awt.Color(54, 149, 255));
-        btnNewBook.setGradientColor2(new java.awt.Color(54, 149, 255));
-        btnNewBook.addActionListener(new java.awt.event.ActionListener() {
+        btnOpenShopCart1.setText("Abrir Carrito");
+        btnOpenShopCart1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnNewBookActionPerformed(evt);
+                btnOpenShopCart1ActionPerformed(evt);
             }
         });
 
-        btnOpenShopCart.setText("Abrir Carrito");
+        btnOpenShopCart.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icon/shopCart.png"))); // NOI18N
+        btnOpenShopCart.setText("1");
+        btnOpenShopCart.setBadgeColor(new java.awt.Color(54, 149, 255));
+        btnOpenShopCart.setOpaque(false);
         btnOpenShopCart.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnOpenShopCartActionPerformed(evt);
@@ -78,20 +77,20 @@ public class MarketPlacePanel extends javax.swing.JPanel {
         panelBackground1Layout.setHorizontalGroup(
             panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelBackground1Layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(btnNewBook, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 692, Short.MAX_VALUE)
-                .addComponent(btnOpenShopCart, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(14, 14, 14))
+                .addGap(15, 15, 15)
+                .addComponent(btnOpenShopCart1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 712, Short.MAX_VALUE)
+                .addComponent(btnOpenShopCart, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(34, 34, 34))
         );
         panelBackground1Layout.setVerticalGroup(
-            panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelBackground1Layout.createSequentialGroup()
-                .addContainerGap(7, Short.MAX_VALUE)
+            panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(panelBackground1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addGroup(panelBackground1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnNewBook, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnOpenShopCart))
-                .addContainerGap())
+                    .addComponent(btnOpenShopCart1)
+                    .addComponent(btnOpenShopCart, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, 0))
         );
 
         portrait.setLayout(new java.awt.GridLayout(0, 4, 6, 23));
@@ -102,11 +101,11 @@ public class MarketPlacePanel extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(35, Short.MAX_VALUE)
+                .addContainerGap(57, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(scroll)
                     .addComponent(panelBackground1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(35, Short.MAX_VALUE))
+                .addContainerGap(58, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -114,21 +113,12 @@ public class MarketPlacePanel extends javax.swing.JPanel {
                 .addGap(20, 20, 20)
                 .addComponent(panelBackground1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(scroll, javax.swing.GroupLayout.PREFERRED_SIZE, 657, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(22, Short.MAX_VALUE))
+                .addComponent(scroll, javax.swing.GroupLayout.PREFERRED_SIZE, 652, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnNewBookActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNewBookActionPerformed
-//        BookView book = new BookView("/Images/portadas/portadaLibro_" + index + ".jpg");
-//        portrait.add(book);
-//        portrait.updateUI();
-//
-//        System.out.println("si estoy funcionando");
-//        index ++;
-    }//GEN-LAST:event_btnNewBookActionPerformed
-
-    private void btnOpenShopCartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOpenShopCartActionPerformed
+    private void btnOpenShopCart1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOpenShopCart1ActionPerformed
 //        if (shoppingCartFrameInstance == null) {
 //            try {
 //                shoppingCartFrameInstance = new ShoppingCartFrame();
@@ -138,6 +128,10 @@ public class MarketPlacePanel extends javax.swing.JPanel {
 //        }
 //        isShowing = !isShowing;
 //        shoppingCartFrameInstance.setVisible(isShowing);
+    }//GEN-LAST:event_btnOpenShopCart1ActionPerformed
+
+    private void btnOpenShopCartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOpenShopCartActionPerformed
+        // TODO add your handling code here:
     }//GEN-LAST:event_btnOpenShopCartActionPerformed
 
     public JButton getBtnOpenShopCart() {
@@ -148,8 +142,8 @@ public class MarketPlacePanel extends javax.swing.JPanel {
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private components.ButtonGradient btnNewBook;
-    private javax.swing.JButton btnOpenShopCart;
+    private components.BadgeButton btnOpenShopCart;
+    private javax.swing.JButton btnOpenShopCart1;
     private components.mainComponents.swing.PanelBackground panelBackground1;
     private components.mainComponents.swing.PanelBackground portrait;
     private javax.swing.JScrollPane scroll;
