73be20dd391e0df939dc2f92903d3045f9723f4b
Cristian Vega <crisda.arive.1503@gmail.com>
2023-07-24 19:32:50 -0500

    Fix in StockDetailsPanel

--- src/app/views/stock/StockDetailsPanel.java
+++ src/app/views/stock/StockDetailsPanel.java
@@ -25,9 +25,15 @@ public class StockDetailsPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel7 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         detailsQuantity = new javax.swing.JSpinner();
+        jLabel8 = new javax.swing.JLabel();
         detailsShelve = new javax.swing.JComboBox<>();
+
+        jLabel7.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabel7.setForeground(java.awt.Color.black);
+        jLabel7.setText("Cantidad de Libros");
 
         setOpaque(false);
 
@@ -35,6 +41,19 @@ public class StockDetailsPanel extends javax.swing.JPanel {
         jLabel6.setForeground(java.awt.Color.black);
         jLabel6.setText("Cantidad de Libros");
 
+        detailsQuantity.setBorder(null);
+        detailsQuantity.setEnabled(false);
+        detailsQuantity.setFocusable(false);
+
+        jLabel8.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabel8.setForeground(java.awt.Color.black);
+        jLabel8.setText("Nombre Estante");
+
+        detailsShelve.setBackground(new java.awt.Color(255, 255, 255));
+        detailsShelve.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 12)); // NOI18N
+        detailsShelve.setForeground(new java.awt.Color(204, 204, 204));
+        detailsShelve.setBorder(null);
+        detailsShelve.setFocusable(false);
         detailsShelve.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 detailsShelveActionPerformed(evt);
@@ -50,24 +69,27 @@ public class StockDetailsPanel extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(detailsQuantity, javax.swing.GroupLayout.DEFAULT_SIZE, 262, Short.MAX_VALUE)
                     .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(detailsShelve, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(detailsShelve, 0, 262, Short.MAX_VALUE))
                 .addContainerGap(31, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(26, 26, 26)
+                .addGap(48, 48, 48)
                 .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(detailsQuantity, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addGap(29, 29, 29)
+                .addComponent(jLabel8)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(detailsShelve, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(237, Short.MAX_VALUE))
+                .addContainerGap(184, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void detailsShelveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_detailsShelveActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_detailsShelveActionPerformed
 
 
@@ -75,5 +97,7 @@ public class StockDetailsPanel extends javax.swing.JPanel {
     private javax.swing.JSpinner detailsQuantity;
     private javax.swing.JComboBox<Shelve> detailsShelve;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     // End of variables declaration//GEN-END:variables
 }
